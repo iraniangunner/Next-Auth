c6fee65ae80fb8429851d44c87173667e9aa81ba
fateh moonesi <f.moonesi@jsk.local>
2024-07-23 16:03:03 +0330

    add delete and update session function

--- app/lib/session.ts
+++ app/lib/session.ts
@@ -38,3 +38,27 @@ import { cookies } from "next/headers";
 //     path: "/",
 //   });
 // }
+
+ 
+// export async function updateSession() {
+//   const session = cookies().get('session')?.value
+//   const payload = await decrypt(session)
+ 
+//   if (!session || !payload) {
+//     return null
+//   }
+ 
+//   const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+//   cookies().set('session', session, {
+//     httpOnly: true,
+//     secure: true,
+//     expires: expires,
+//     sameSite: 'lax',
+//     path: '/',
+//   })
+// }
+
+ 
+// export function deleteSession() {
+//   cookies().delete('session')
+// }
