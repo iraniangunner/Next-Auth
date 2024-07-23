cb0c3a4f4585797b4402d892f0af51909fe0911d
fateh moonesi <f.moonesi@jsk.local>
2024-07-23 15:51:49 +0330

    some files added

---
+++ app/lib/definitions.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+
+export const SignupFormSchema = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Name must be at least 2 characters long." })
+    .trim(),
+  email: z.string().email({ message: "Please enter a valid email." }).trim(),
+  password: z
+    .string()
+    .min(8, { message: "Be at least 8 characters long" })
+    .regex(/[a-zA-Z]/, { message: "Contain at least one letter." })
+    .regex(/[0-9]/, { message: "Contain at least one number." })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: "Contain at least one special character.",
+    })
+    .trim(),
+});
+
+export type FormState =
+  | {
+      errors?: {
+        name?: string[];
+        email?: string[];
+        password?: string[];
+      };
+      message?: string;
+    }
+  | undefined;
+
+// export const SessionPayloadSchema = z.object({
+//   userId: z.string(),
+//   //   sessionId: z.string(),
+//   expiresAt: z.string().refine((date) => !isNaN(Date.parse(date)), {
+//     message: "Expiry must be a valid date string.",
+//   }),
+// });
+
+// // Export the Session Payload Schema
+// export type SessionPayload = z.infer<typeof SessionPayloadSchema>;
