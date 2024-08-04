d460c176c5aed4a67e1fb86dcf072bc512b9aebd
fateh moonesi <f.moonesi@jsk.local>
2024-08-04 16:22:28 +0330

    try/catch block added!

--- actions/login.ts
+++ actions/login.ts
@@ -8,26 +8,33 @@ export async function login(prevState: any, formData: FormData) {
   const password = formData.get("password");
 
   //2.Send credentials to server
-  const response = await fetch("https://dummyjson.com/auth/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      username,
-      password,
-    }),
-  });
 
-  if (!response.ok) {
+  try {
+    const response = await fetch("https://dummyjson.com/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+
+    if (!response.ok) {
+      return {
+        message: "An error occurred while login your account.",
+      };
+    }
+
+    //3.get user information from server
+    const user = await response.json();
+
+    //4.create cookie session and save token
+    await createSession(user.token);
+  } catch (error) {
     return {
-      message: "An error occurred while login your account.",
+      message: "Something goes wrong!",
     };
   }
-
-  //3.get user information from server
-  const user = await response.json();
-
-  //4.create cookie session and save token
-  await createSession(user.token);
 
   //5.redirect to dashboard
   redirect("/");
